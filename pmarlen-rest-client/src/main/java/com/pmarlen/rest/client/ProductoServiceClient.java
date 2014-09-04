c890777ded67bce6fba57e557a242eb150e5bbb4
alfredo estrada <alfredo@MacBook-Pro-de-mac.local>
2014-09-04 10:58:31 -0500

    fixes in mac

--- pmarlen-rest-client/src/main/java/com/pmarlen/rest/client/ProductoServiceClient.java
+++ pmarlen-rest-client/src/main/java/com/pmarlen/rest/client/ProductoServiceClient.java
@@ -10,40 +10,40 @@ import com.google.gson.*;
 
 public class ProductoServiceClient {
 
-    public static void main(String[] args) {
-        try {
+	public static void main(String[] args) {
+		try {
 
-            Client client = Client.create();
+			Client client = Client.create();
 			Gson gson=new Gson();
 
-            WebResource webResource = client.resource("http://localhost:8070/pmarlen-rest-services/rs/producto/getAll");
+			WebResource webResource = client.resource("http://localhost:8070/pmarlen-rest-services/rs/producto/getAll");
 
-            ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
 
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatus());
-            }
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
 
-            String output = response.getEntity(String.class);
+			String output = response.getEntity(String.class);
 
-            //System.out.println("Output from Server .... \n");
-            //System.out.println(output);
+			//System.out.println("Output from Server .... \n");
+			//System.out.println(output);
 
 			Producto[] productoRead = new Producto[1];
 			productoRead = gson.fromJson(output, productoRead.getClass());
 
 			System.out.println("productoRead:");
-            for(Producto pX: productoRead){
+			for(Producto pX: productoRead){
 				System.out.println("\t->Producto:"+pX.getId()+", ["+pX.getCodigoBarras()+"]");
 			}
 
-        } catch (Exception e) {
+		} catch (Exception e) {
 
-            e.printStackTrace();
+			e.printStackTrace();
 
-        }
+		}
 
-    }
+	}
 
 }
